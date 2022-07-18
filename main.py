c3478b3bfc3e83396636c4e96c9d44b23372ec62
Sanket Sonu <sanketsonu3@gmail.com>
2022-07-18 15:23:07 +0100

    1st Version

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append('fast-coref/src')
+from inference.model_inference import Inference
+
+def replace():
+    # For joint model
+    inference_model = Inference("./", encoder_name="shtoshni/longformer_coreference_joint")
+
+    doc = "My sister has a dog. She loves him."
+
+    output = inference_model.perform_coreference(doc)
+
+    main_list = list()
+    for cluster in output["clusters"]:
+      if len(cluster) > 1:
+        each_cluster=list()
+        for each in cluster:
+            each_cluster.append(each[1])
+        main_list.append(each_cluster)
+
+        print(main_list)
+
+    # for i in main_list:
+    #     if len(main_list) > 1:
+    #
+    #         for x in main_list:
+    #             doc = doc.replace(main_list[0][1],main_list[0][0])
+    # print(doc)
+
+
+    # Creating a dictionary from clusters, where Key will be the first word of each list of list and rest other items will be values.
+
+    dict = {}
+
+    for x in main_list:
+        dict[x[0]] = x[1:]
+
+    print(dict)
+
+    # Now, we have to create a logic which uses dictionary to scan input doc and replace all values with a key of the dictionary.
+
+    # Tried this but didnt got correct output. Check this logic:
+    for word, replacement in dict.items():
+        doc = doc.replace(str(word), str(replacement))
+    print(doc)
+
+replace()
